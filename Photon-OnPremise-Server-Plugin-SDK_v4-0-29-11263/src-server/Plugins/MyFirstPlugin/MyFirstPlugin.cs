0416900f6b27d511dd69465041f4790099ec6856
Unknown <193541T@mymail.nyp.edu.sg>
2021-05-30 16:10:25 +0800

    hmmm
    removed copy of PhotonHivePlugin.dll

---
+++ Photon-OnPremise-Server-Plugin-SDK_v4-0-29-11263/src-server/Plugins/MyFirstPlugin/MyFirstPlugin.cs
@@ -0,0 +1,51 @@
+﻿using Photon.Hive.Plugin;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstPlugin {
+    public class MyFirstPlugin: PluginBase {
+		public override string Name => "MyFirstPlugin"; //The reserved plugin names are "Default" and "ErrorPlugin"
+
+		//private IPluginLogger pluginLogger;
+
+		//public override bool SetupInstance(IPluginHost host, Dictionary<string, string> config, out string errorMsg)
+		//{
+		//	this.pluginLogger = host.CreateLogger(this.Name);
+		//	return base.SetupInstance(host, config, out errorMsg);
+		//}
+
+		public override void OnCreateGame(ICreateGameCallInfo info) {
+            this.PluginHost.LogInfo(string.Format("OnCreateGame {0} by user {1}", info.Request.GameId, info.UserId));
+            info.Continue(); // same as base.OnCreateGame(info);
+        }
+
+		public override void OnRaiseEvent(IRaiseEventCallInfo info) {
+			base.OnRaiseEvent(info);
+
+			if(info.Request.EvCode == 1) {
+				string request = Encoding.Default.GetString((byte[])info.Request.Data);
+				string response = "Message Received: " + request;
+
+				PluginHost.BroadcastEvent(
+					target: ReciverGroup.All,
+					senderActor: 0,
+					targetGroup: 0,
+					data: new Dictionary<byte, object>() { { (byte)245, response } },
+					evCode: info.Request.EvCode,
+					cacheOp: 0
+				);
+			}
+		}
+	}
+
+    public class MyPluginFactory: IPluginFactory {
+        public IGamePlugin Create(IPluginHost gameHost, string pluginName, Dictionary<string, string> config,
+            out string errorMsg) {
+            MyFirstPlugin plugin = new MyFirstPlugin();
+            if(plugin.SetupInstance(gameHost, config, out errorMsg)) {
+                return plugin;
+            }
+            return null;
+        }
+    }
+}
